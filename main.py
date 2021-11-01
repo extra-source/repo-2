49b9d714277a9da7adcb2bcb5f6117c2b7af6784
extra-source <90742621+extra-source@users.noreply.github.com>
2021-11-01 14:12:14 -0400

    add

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def main(amount):
     for i in range(amount):
         planted_crop = cultivate.plant()
         print('\n', '-'*50)
-        os.system(f'git commit -am "#{planted_crop}"')
-        os.system('git push --all')
+        # os.system(f'git commit -am "#{planted_crop}"')
+        # os.system('git push --all')
         time.sleep(0.1)
 
 crop_amount = int(input('Crop Amount: '))
